7c074ee68453eca9c08bd3ab9cab1bc54fec9271
AndreyFromHome <andreyarapov159@gmail.com>
2022-11-28 14:21:27 +0500

    in process...

--- app/src/main/java/com/example/crystalball/MainActivity.kt
+++ app/src/main/java/com/example/crystalball/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.crystalball
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.VpnService.prepare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +12,7 @@ import com.example.crystalball.data.Dictionary
 import com.example.crystalball.data.DictionaryItem
 import com.example.crystalball.databinding.ActivityMainBinding
 import com.example.crystalball.model.apis.ApiInterface
+import kotlinx.coroutines.NonCancellable.start
 import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvPhonetic : TextView
     lateinit var tvWordResult : TextView
     lateinit var btGetWord : Button
+
+    lateinit var getWord : String
+    lateinit var getSoundUrl : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,19 +43,33 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        // При нажатии на кнопку надо получить по API слово Android
-        binding.btGetWord.setOnClickListener {
-            getWordFromApi()
-        }
-
         var listOfRandomWords = listOf(
             "Apple",
             "Android",
             "Hello",
-            "Declare"
+            "Declare",
+            "Shuffle"
         )
 
-        binding.tvPhonetic.text = listOfRandomWords.get(2)
+        getSoundUrl = "https://ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
+
+        // При нажатии на кнопку надо получить по API слово Android
+        binding.btGetWord.setOnClickListener {
+
+            getWord = listOfRandomWords.random()
+            getWordFromApi()
+
+        }
+
+
+
+
+
+     //   var listOfRandomWordsSize = listOfRandomWords.size - 1
+     //   var listOfRandomWordsRandom = (0..listOfRandomWordsSize).random()
+
+        // binding.tvPhonetic.text = listOfRandomWords.get(3)
+
     }
 
 
@@ -70,7 +91,7 @@ class MainActivity : AppCompatActivity() {
     // Случайные слова для подставноки в API берутся из локальной БД
 
     private fun getWordFromApi() {
-        val apiInterface = ApiInterface.create().getDictionaryItem()
+        val apiInterface = ApiInterface.create().getDictionaryItem(getWord)
         apiInterface.enqueue(object : Callback<List<DictionaryItem>> {
             override fun onResponse(
                 call: Call<List<DictionaryItem>>?,
@@ -79,10 +100,13 @@ class MainActivity : AppCompatActivity() {
 //                Log.d("MyLog", "Ответ от API: $response")
                 if (response?.body() != null) {
                     val responseList = response?.body()
-                    responseList?.forEach {
-                     //   Log.d("MyLog", "${it.word}")
-                        binding.tvWord.text = it.word
+                    responseList?.forEach { it ->
+                        //   Log.d("MyLog", "${it.word}")
+                        binding.tvWord.text = it.word + getSoundUrl
                         binding.tvPhonetic.text = it.phonetic
+                        it.phonetics.forEach {
+                            getSoundUrl = it.audio
+                        }
                     }
                 }
             }
@@ -93,6 +117,19 @@ class MainActivity : AppCompatActivity() {
                 binding.tvWord.text = "Что-то не так"
             }
         })
+
+        val url = getSoundUrl // your URL here
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
+        }
     }
 
 
--- app/src/main/java/com/example/crystalball/model/apis/ApiInterface.kt
+++ app/src/main/java/com/example/crystalball/model/apis/ApiInterface.kt
@@ -5,10 +5,11 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiInterface {
-    @GET("api/v2/entries/en/android")
-    fun getDictionaryItem() : Call<List<DictionaryItem>>
+    @GET("api/v2/entries/en/{getWord}")
+    fun getDictionaryItem(@Path("getWord") getWord: String) : Call<List<DictionaryItem>>
 
     companion object {
 
