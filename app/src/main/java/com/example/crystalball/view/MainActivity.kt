0a7db750bf5abe79e4a60143d43eb583460a132b
AndreyFromHome <andreyarapov159@gmail.com>
2022-11-30 13:04:28 +0500

    in process... add new fields in top-100

--- app/src/main/java/com/example/crystalball/view/MainActivity.kt
+++ app/src/main/java/com/example/crystalball/view/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.example.crystalball.view
 
-import android.media.AudioAttributes
-import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.crystalball.R
-import com.example.crystalball.data.DictionaryItem
-import com.example.crystalball.data.wordsList
 import com.example.crystalball.databinding.ActivityMainBinding
-import com.example.crystalball.model.apis.ApiInterface
 import com.example.crystalball.view.adapters.ViewPagerAdapter
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
 
     private val fragList = listOf(
         RandomPhraseFragment.newInstance(),
@@ -30,21 +20,10 @@ class MainActivity : AppCompatActivity() {
         "ТОП-100 слов"
     )
 
-    lateinit var binding : ActivityMainBinding
-
-    private lateinit var tvWord : TextView
-    private lateinit var tvPhonetic : TextView
-    lateinit var tvWordResult : TextView
-
-    lateinit var randomWord : String
-    lateinit var getSoundUrl : String
-
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.tvWord.text = ""
-        binding.tvPhonetic.text = ""
 
         val vpAdapter = ViewPagerAdapter(this, fragList)
         binding.vp2.adapter = vpAdapter // устанавливаем адаптер в наш viewPager
@@ -52,89 +31,6 @@ class MainActivity : AppCompatActivity() {
             tab, pos -> tab.text = fragListTitles[pos]
         }.attach()
 
-
-
-        var listOfRandomWords = listOf(
-            "Inch",
-            "Apple",
-            "Android",
-            "Chat",
-            "Hello",
-            "Declare",
-            "Shuffle"
-        )
-
-        getSoundUrl = "https://ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
-
-
-        // При нажатии на кнопку надо получить по API слово Android
-        binding.btGetWord.setOnClickListener {
-
-            randomWord = listOfRandomWords.random()
-            getWordFromApi(randomWord)
-            Log.d("MyLog", "Жмём на кнопку и отправляем слово: $randomWord")
-
-        }
-
-
-    }
-
-    // Получение полной информации о слове с API https://dictionaryapi.dev/
-    // Случайные слова для подставноки в API берутся из локальной БД
-
-    private fun getWordFromApi(getWord: String) {
-        val apiInterface = ApiInterface.create().getDictionaryItem(getWord)
-        apiInterface.enqueue(object : Callback<List<DictionaryItem>> {
-            override fun onResponse(
-                call: Call<List<DictionaryItem>>?,
-                response: Response<List<DictionaryItem>>?
-            ) {
-
-                if (response?.body() != null) {
-                    val responseList = response?.body()
-                    responseList?.forEach { it ->
-                        //   Log.d("MyLog", "${it.word}")
-                        getSoundUrl = it.phonetics[0].audio
-
-                        playSound(getSoundUrl)
-
-                        binding.tvWord.text = it.word
-                        binding.tvPhonetic.text = it.phonetic
-
-
-                        Log.d("MyLog", "Получена ссылка $getSoundUrl \n Получено слово: ${it.word}")
-/*                            .forEach {
-                            getSoundUrl = it.audio.
-                            Log.d("MyLog", "Получена ссылка $getSoundUrl")
-                        }*/
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<DictionaryItem>>?, t: Throwable?) {
-                // do nothing
-                // Log.d("MyLog", "Ошибка: $t")
-                binding.tvWord.text = "Что-то не так"
-            }
-        })
-
-
-    }
-
-    fun playSound(setUrlForPlayer: String) {
-        val url = setUrlForPlayer // your URL here
-        val mediaPlayer = MediaPlayer().apply {
-            setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .build()
-            )
-            setDataSource(url)
-            prepare() // might take long! (for buffering, etc)
-            start()
-            Log.d("MyLog", "Слушаем слово $getSoundUrl")
-        }
     }
 
 }
--- app/src/main/java/com/example/crystalball/view/Top100WordsFragment.kt
+++ app/src/main/java/com/example/crystalball/view/Top100WordsFragment.kt
@@ -1,17 +1,30 @@
 package com.example.crystalball.view
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.crystalball.R
+import android.widget.TextView
+import com.example.crystalball.data.DictionaryItem
 import com.example.crystalball.databinding.FragmentTop100WordsBinding
+import com.example.crystalball.model.apis.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Top100WordsFragment : Fragment() {
 
     lateinit var binding: FragmentTop100WordsBinding
+
+    lateinit var randomWord : String
+    lateinit var getSoundUrl : String
+    private lateinit var tvWord : TextView
+    private lateinit var tvPhonetic : TextView
+    lateinit var tvWordResult : TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,15 +36,93 @@ class Top100WordsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btAction.setOnClickListener {
-            Log.d("MyLog", "Нажал кнопку btAction")
+
+        // Начальные значение переменных и view
+        binding.tvWord.text = ""
+        binding.tvPhonetic.text = ""
+        getSoundUrl = "https://ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
+
+        var listOfRandomWords = listOf(
+            "Inch",
+            "Apple",
+            "Android",
+            "Chat",
+            "Hello",
+            "Declare",
+            "Shuffle"
+        )
+
+        // При нажатии на кнопку надо получить по API слово Android
+        binding.btGetWord.setOnClickListener {
+            randomWord = listOfRandomWords.random()
+            getWordFromApi(randomWord)
+            Log.d("MyLog", "Жмём на кнопку и отправляем слово: $randomWord")
         }
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance() = Top100WordsFragment()
+    }
 
+    // Получение полной информации о слове с API https://dictionaryapi.dev/
+    // Случайные слова для подставноки в API берутся из локальной БД
+
+    private fun getWordFromApi(getWord: String) {
+        val apiInterface = ApiInterface.create().getDictionaryItem(getWord)
+        apiInterface.enqueue(object : Callback<List<DictionaryItem>> {
+            override fun onResponse(
+                call: Call<List<DictionaryItem>>?,
+                response: Response<List<DictionaryItem>>?
+            ) {
+
+                if (response?.body() != null) {
+                    val responseList = response?.body()
+                    responseList?.forEach { it ->
+                        //   Log.d("MyLog", "${it.word}")
+                        getSoundUrl = it.phonetics[0].audio
+
+                        playSound(getSoundUrl)
+
+                        binding.tvWord.text = it.word
+                        binding.tvPhonetic.text = it.phonetic
+/*                        it.meanings.forEach{
+                            it.definitions.forEach{
+                                it.definition
+                            }
+                        }*/
+                        val definition = it.meanings[0].definitions[0].definition
+                        definition?.let {
+                            binding.tvDefinition.text = "Определение (definition): \n" + definition
+                        }
+
+                        Log.d("MyLog", "Получена ссылка $getSoundUrl \n Получено слово: ${it.word}")
+
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<DictionaryItem>>?, t: Throwable?) {
+                // do nothing
+                // Log.d("MyLog", "Ошибка: $t")
+                binding.tvWord.text = "Что-то не так"
+            }
+        })
+    }
+
+    fun playSound(setUrlForPlayer: String) {
+        val url = setUrlForPlayer // your URL here
+        val mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
+        //    Log.d("MyLog", "Слушаем слово $getSoundUrl")
+        }
     }
 }
